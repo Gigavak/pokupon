88cf98d290d7acf7e0f202649d6152cb4a7933fe
Gigavak <gigavak@gmail.com>
2017-11-23 10:11:24 +0200

    some changes

--- src/test/java/someTests/newClass.java
+++ src/test/java/someTests/newClass.java
@@ -1,4 +1,34 @@
 package someTests;
 
 public class newClass {
+    double width;
+    double height;
+    double depth;
+
+    double volume() {
+        return width * height * depth;
+    }
+
+    void setDim(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
+    }
 }
+    class BoxDemo1{
+        public static void main(String[] args) {
+            newClass newBox = new newClass();
+            newClass newBox2 = new newClass();
+            double vol;
+
+            newBox.setDim(10,20,15);
+            newBox2.setDim(3,6,9);
+
+            vol = newBox.volume();
+            System.out.println("vol "+ vol);
+
+            vol = newBox2.volume();
+            System.out.println("vol" + vol);
+        }
+    }
+
