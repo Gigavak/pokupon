1a33148dbf141a44d153a3ca80f8481a15dd8217
Gigavak <gigavak@gmail.com>
2017-12-01 16:25:35 +0200

    new class

--- src/test/java/someTests/HelloWorldRestAssured.java
+++ src/test/java/someTests/HelloWorldRestAssured.java
@@ -3,6 +3,7 @@ package someTests;
 import org.testng.annotations.Test;
 
 import static com.jayway.restassured.RestAssured.given;
+import com.nitorcreations.Matchers;
 
 
 public class HelloWorldRestAssured {
@@ -11,7 +12,7 @@ public class HelloWorldRestAssured {
     public void makeSureThatGoogleIsUp() {
 
         given().when().get("https://apigermany.pokupon.ua/v1/comments").then().statusCode(200);
-
+        given().when().get("https://apigermany.pokupon.ua/v1/comments").then().assertThat().body("MRData.CircuitTable.Circuits.circuitId", hasSize(20));
     }
 
 }
