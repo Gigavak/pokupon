49f56e572e541f2e682f729a1d4807ebcb0b5428
Gigavak <gigavak@gmail.com>
2017-12-14 12:12:04 +0200

    Merge remote-tracking branch 'remotes/origin/develop' into local

--- src/test/java/pokupon/autotest/tests/CreatePurchaseCouponTest.java
+++ src/test/java/pokupon/autotest/tests/CreatePurchaseCouponTest.java
@@ -25,7 +25,7 @@ public class CreatePurchaseCouponTest {
 
         createPurchaseCoupon = new CreatePurchaseCoupon(driver);
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(145, TimeUnit.SECONDS);
         driver.get("https://pokupon.ua/users/sign_in");
     }
 
